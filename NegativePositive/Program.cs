dd22a401c09abcc29a00b9ac43e37d87cc85ec04
ИСП-22 <-22@LAB30-09>
2023-10-10 08:05:18 +0300

    Добавьте файлы проекта.

---
+++ NegativePositive/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int pos = 0;
+int neg = 0;
+
+if (a > 0) pos++; else if (a < 0) neg++;
+if (b > 0) pos++; else if (b < 0) neg++;
+if (c > 0) pos++; else if (c < 0) neg++;
+
+Console.WriteLine("Положительных: " + pos);
+Console.WriteLine("Отрицательных: " + neg);
