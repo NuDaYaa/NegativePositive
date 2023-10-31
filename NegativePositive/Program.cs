492fc93fc7a5c934ac42ca65421d0c4e1eed044a
enrodin <145424997+enrodin@users.noreply.github.com>
2023-10-31 08:27:38 +0300

    Update Program.cs 666666666666666666666666666666

--- NegativePositive/Program.cs
+++ NegativePositive/Program.cs
@@ -1,16 +1,39 @@
 ﻿
 using System;
 
-int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
-int c = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine()),
+    b = int.Parse(Console.ReadLine()),
+    c = int.Parse(Console.ReadLine());
 
-int pos = 0;
-int neg = 0;
+int pos = 0,
+    neg = 0;
 
-if (a > 0) pos++; else if (a < 0) neg++;
-if (b > 0) pos++; else if (b < 0) neg++;
-if (c > 0) pos++; else if (c < 0) neg++;
+if (a > 0) 
+{
+  pos++;
+} 
+else if (a < 0) 
+{
+  neg++;
+}
 
-Console.WriteLine("Положительных: " + pos);
-Console.WriteLine("Отрицательных: " + neg);
+if (b > 0) 
+{
+  pos++; 
+}
+else if (b < 0) 
+{
+  neg++;
+}
+
+if (c > 0) 
+{
+  pos++; 
+}
+else if (c < 0) 
+{
+  neg++;
+}
+
+Console.WriteLine($"Положительных: {pos}");
+Console.WriteLine($"Отрицательных: {neg}");
